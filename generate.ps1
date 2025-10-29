9e16f0c6e50ea9ed5ffbb6818b8e5f2028530d5c
Sri2103 <damasriharsha@gmail.com>
2025-10-29 17:35:22 +0530

    first generated deepcopy file

---
+++ generate.ps1
@@ -0,0 +1,3 @@
+deepcopy-gen.exe --input-dirs="./pkg/apis/platform/v1alpha1" --output-base="." --go-header-file=".\hack\boiler-plate.go.txt" -v 5
+
+
---
+++ pkg/apis/platform/v1alpha1/types.go
@@ -0,0 +1,41 @@
+package v1alpha1
+
+// +kubebuilder:object:generate=true
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ResourceQuotaPolicySpec defines the desired state
+type ResourceQuotaPolicySpec struct {
+	MaxPods     int    `json:"maxPods,omitempty"`
+	MaxServices int    `json:"maxServices,omitempty"`
+	MaxCPU      string `json:"maxCPU,omitempty"`
+	MaxMemory   string `json:"maxMemory,omitempty"`
+}
+
+// ResourceQuotaPolicyStatus defines observed usage
+type ResourceQuotaPolicyStatus struct {
+	CurrentPods     int      `json:"currentPods,omitempty"`
+	CurrentServices int      `json:"currentServices,omitempty"`
+	CPUUsage        string   `json:"cpuUsage,omitempty"`
+	MemoryUsage     string   `json:"memoryUsage,omitempty"`
+	Violations      []string `json:"violations,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ResourceQuotaPolicy struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ResourceQuotaPolicySpec   `json:"spec,omitempty"`
+	Status ResourceQuotaPolicyStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ResourceQuotaPolicyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ResourceQuotaPolicy `json:"items"`
+}
